9a0ebc50aa5b12bcdf523b52cce96de16dca0c03
AleksandrBud <budzin-a@yandex.ru>
2021-04-27 12:22:41 +0500

    добвил 2 режим работы, работа по событию.

--- tlg.py
+++ tlg.py
@@ -11,12 +11,14 @@ import proc_films
 import proc_book
 import proc_news
 import demoji
-# import asyncio
 import re
+import logging
 from telethon.sync import TelegramClient
-from telethon import events, functions
+from telethon import events
+    # , functions
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from time import strftime
 
 
 async def get_messages_from_chat(dialog: object, lst_date, category: str):
@@ -64,11 +66,15 @@ async def parce_maessages(msg_list: list, dialog_id: int, category: str, folder:
                     try:
                         await globals()['task_%s' % n]
                         fn = globals()['class_%s' % n]['name']
+                        curr_date = strftime('[%Y-%b-%d %H:%M:%S]')
+                        # logger.info(f'{curr_date} Complate {fn}')
                         print(f'Complate {fn}')
                     except Exception as e:
+                        curr_date = strftime('[%Y-%b-%d %H:%M:%S]')
+                        # logger.error(f"{curr_date} ERROR {dialog_id} {globals()['mess_id%s' % n].id}, {e.__str__()}")
                         print(dialog_id, globals()['mess_id%s' % n].id, '1', e.__str__())
-                        await proc_error.process_exeption(dialog_id, globals()['mess_id%s' % n].id, '1', e.__str__(),
-                                                          session)
+                        # await proc_error.process_exeption(dialog_id, globals()['mess_id%s' % n].id, '1', e.__str__(),
+                        #                                   session)
                 i = 0
             try:
                 # Создаем задачу на загрузку файлов
@@ -151,7 +157,7 @@ async def main():
             # Чат есть в списке пошли смотреть сообщения
             type_chat = proc_chat.get_type(dialog.id, session)
             # proc_list = ['music', 'book', 'app']
-            proc_list = ['news']
+            proc_list = ['news', 'music', 'book']
             if type_chat in proc_list:
                 main_path = '/home/aleksandr/Загрузки/'
                 path_for_save = main_path + type_chat + '/'
@@ -166,13 +172,22 @@ if __name__ == '__main__':
         'https': 'socks5://localhost:9050'
     }
     config = configparser.ConfigParser()
-    config.read('config.ini')
+    config.read('/home/aleksandr/Yandex.Disk/github/TLG/config.ini')
     api_id = config['Telegram']['api_id']
     api_hash = config['Telegram']['api_hash']
     username = config['Telegram']['username']
     proxy_server = config['Telegram']['proxy_server']
     proxy_port = config['Telegram']['proxy_port']
     proxy_key = config['Telegram']['proxy_key']
+    main_path = '/home/aleksandr/Загрузки/'
+    logging.FileHandler
+    # handler = logging.handlers.RotatingFileHandler('error.log',
+    #                                                maxBytes=500000,
+    #                                                backupCount=5)
+    # logger = logging.getLogger(__name__)
+    # logger.setLevel(logging.INFO)
+    # logger.addHandler(handler)
+
     demoji.download_codes()
     engine = create_engine(con_db.connect_string())
     session_maker = sessionmaker(bind=engine)
@@ -182,19 +197,31 @@ if __name__ == '__main__':
                            # proxy=(socks.SOCKS5, 'localhost', 9050)  # (socks.SOCKS5, '127.0.0.1', 9150)
                             )
 
+    mode_run_program = input('Choice mode run program (event(press e)/last messages(press l)): ')
+    if mode_run_program == 'e':
+        @client.on(events.NewMessage())
+        async def normal_handler(event):
+            #    print(event.message)
+            # print(event.message.to_dict()['message'])
+            chat_id = event.chat_id
+            if proc_chat.unique_record(chat_id, session):
+                type_chat = proc_chat.get_type(chat_id, session)
+                proc_list = ['news', 'music', 'book']
+                if type_chat in proc_list:
+                    msg_list = [event.message]
+                    path_for_save = main_path + type_chat + '/'
+                    await parce_maessages(msg_list, chat_id, type_chat, path_for_save)
+                    try:
+                       await event.message.mark_read()
+                    except Exception as e:
+                        print(e)
 
-    # @client.on(events.NewMessage())
-    # async def normal_handler(event):
-    #     #    print(event.message)
-    #     print(event.message.to_dict()['message'])
-    #     # m = await event.respond('!pong')
-    #     await asyncio.sleep(5)
-    #     # await client.delete_messages(event.chat_id, [event.id, m.id])
-    #     await client.send_read_acknowledge(entity=event.chat_id, message=event.id, max_id=event.id, clear_mentions=True)
-    # client.start()
-    # client.run_until_disconnected()
-    with client:
-        client.loop.run_until_complete(main())
-    print('END')
 
-# https://t.me/tass_agency     # client.disconnect()
+        client.start()
+        client.run_until_disconnected()
+    elif mode_run_program == 'l':
+        with client:
+            client.loop.run_until_complete(main())
+        print('END')
+    else:
+        print('Input mode not found!')
